7005ada008cb1fd3a61ead97953e0d82f4a0d369
Pirgit Tiislar <ptiislar@tktk.ee>
2021-09-29 14:38:03 +0300

    Control Flow

---
+++ UseAge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UseAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis
+            //"oled sündinud aastal {yearOfBirth}
+
+            Console.WriteLine("Palun, sisesta enda vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - userAge;
+
+            Console.WriteLine($"oled sündinud aastal {yearOfBirth}.");
+        }
+    }
+}
